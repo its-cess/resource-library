ab742e69f6e051a3f7700358dc28ff2f4b486a57
Cecily Toro <torocecily@gmail.com>
2022-09-30 16:18:30 -0400

    deploy sh file

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com/its-cess/resource-library.git master:gh-pages
+git push -f git@github.com:its-cess/resource-library.git main:gh-pages
 cd -
