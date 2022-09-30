365af84d5762ee43ff4f475f475ebef9f7fb0a97
Cecily Toro <torocecily@gmail.com>
2022-09-30 14:41:14 -0400

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <the-header title="Resource Library"></the-header>
+  <the-resources></the-resources>
+</template>
+
+<script>
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheResources,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+  color: #232f34;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
