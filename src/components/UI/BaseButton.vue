b4f82c6e93c299d3d72720920ba1615d854ce819
Cecily Toro <torocecily@gmail.com>
2022-09-30 16:08:49 -0400

    Update

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['mode'],
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #4a6572;
+  border: 1px solid #4a6572;
+  border-radius: 12px;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #232f34;
+  border-color: #232f34;
+}
+
+.flat {
+  background-color: transparent;
+  color: #232f34;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  color: #f9aa33;
+  background-color: #4a6572;
+}
+</style>
